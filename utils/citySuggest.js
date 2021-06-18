05271142368b87b9356ee143dccd28727eee723f
kevin_morize <webdevbox91@gmail.com>
2021-06-18 15:57:21 +0200

    work on city suggest

---
+++ utils/citySuggest.js
@@ -0,0 +1,44 @@
+const search = document.getElementById('citySearch');
+const matchList = document.getElementById('cityList');
+
+search.addEventListener('keyup', () => searchMarks(search.value));
+
+const searchMarks = async searchText => {
+    const res = await fetch('https://geo.api.gouv.fr/communes');
+    const city = await res.json();
+    
+    let matches = city.filter(match => {
+        const regex = new RegExp(`^${searchText}`, 'gi')
+        return match.nom.match(regex) || match.code.match(regex)
+    })
+
+    console.log(matches)
+
+    if (searchText === ""){
+      matchList.innerHTML = "";
+      matches = [];
+    }
+    outputHtml(matches);
+};
+
+const outputHtml = (matches) => {
+    if (matches.length > 0) {
+        var html = matches.map(match =>
+            match.codesPostaux.forEach(code =>
+            `
+            <div class="cityMatch" onclick="selectedCity('${match.nom}', '${match.code}')">
+              ${match.nom} - ${match.code}
+            </div> 
+            `
+            )
+        ).join(''); 
+
+        matchList.innerHTML = html;
+    };           
+}
+
+function selectedCity (nom, codePostaux) {
+      search.value = nom + " - " + codePostaux
+      matchList.innerHTML = "";
+      matches = [];
+}
